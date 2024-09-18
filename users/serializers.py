bbf7d10196d3564530b06fa9bca0c0fa9f260ef8
Thalisu <thalisondmcc@gmail.com>
2024-09-18 15:43:10 -0300

    feat: add username field to UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class UserSerializer(serializers.Serializer):
+    username_validator = UnicodeUsernameValidator()
+
     id = serializers.IntegerField(read_only=True)
     email = serializers.EmailField(max_length=127)
+    username = serializers.CharField(
+        max_length=150, validators=[username_validator]
+    )
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     is_employee = serializers.BooleanField(default=False)

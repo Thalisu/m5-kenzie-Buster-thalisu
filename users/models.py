d8585807c9c4c48963de318415df5ff5ca39f746
Thalisu <thalisondmcc@gmail.com>
2024-09-18 14:56:12 -0300

    feat: add custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=127)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True, default=None)
+    is_employee = models.BooleanField(default=False)
+    username = None
+    is_staff = None
+    is_active = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self, email, password, is_employee, **extra_fields
+    ):
+        if not email:
+            raise ValueError("O email é obrigatório")
+
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_superuser", is_employee)
+
+        user = self.model(email=email, is_employee=is_employee, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user

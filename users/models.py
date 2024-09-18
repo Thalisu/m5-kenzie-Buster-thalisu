b404c487bc8afe8aad747ef61e10b404ed75973d
Thalisu <thalisondmcc@gmail.com>
2024-09-18 15:37:41 -0300

    feat: add username

--- users/models.py
+++ users/models.py
@@ -2,24 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True, max_length=127)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    birthdate = models.DateField(null=True, default=None)
-    is_employee = models.BooleanField(default=False)
-    username = None
-    is_staff = None
-    is_active = None
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
-
-
 class UserManager(BaseUserManager):
-    def create_user(
-        self, email, password, is_employee, **extra_fields
-    ):
+    def create_user(self, email, password, is_employee, **extra_fields):
         if not email:
             raise ValueError("O email é obrigatório")
 
@@ -30,3 +14,18 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=127)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True, default=None)
+    is_employee = models.BooleanField(default=False)
+    is_staff = None
+    is_active = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = UserManager()

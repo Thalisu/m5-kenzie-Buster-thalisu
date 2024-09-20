4e4f20ff073c27afadfa1bf3d15b4d88603f5956
Thalisu <thalisondmcc@gmail.com>
2024-09-20 11:18:38 -0300

    feat: add movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Ratings(models.TextChoices):
+    G = "G"
+    PG = "PG"
+    PG_13 = "PG-13"
+    R = "R"
+    NC_17 = "NC-17"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=127)
+    duration = models.CharField(max_length=10, blank=True, default="")
+    rating = models.CharField(
+        choices=Ratings.choices, max_length=5, default=Ratings.G
+    )
+    synopsis = models.TextField(blank=True, default="")
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="movies"
+    )

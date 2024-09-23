7d31e7c13d0acf57b74bbb6177bc5586d1a257cc
Thalisu <thalisondmcc@gmail.com>
2024-09-23 15:48:30 -0300

    feat: add movie_orders model

--- movies_orders/models.py
+++ movies_orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class MovieOrder(models.Model):
+    purchased_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
